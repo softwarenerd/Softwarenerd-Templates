84b84e53e2b0a8a3d228ad6c26416f4fa084d121
Brian Lambert <brianlambert@gmail.com>
2014-08-01 20:30:43 -0400

    Updates.

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates
 rm -rf ~/Library/Developer/Xcode/Templates/File\ Templates/Softwarenerd
 ln -s $PWD ~/Library/Developer/Xcode/Templates/File\ Templates/Softwarenerd
