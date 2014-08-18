9bc5b5ce7e1d67523dbfed6c06550df2fbbb90bb
Brian Lambert <brianlambert@gmail.com>
2014-08-18 16:18:13 -0400

    Here we go! Ripping up the last of the Intrepid Loser Interface.

---
+++ View.xctemplate/UIView/___FILEBASENAMEASIDENTIFIER___Delegate.h
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <Foundation/Foundation.h>
+
+// Forward declarations.
+@class ___FILEBASENAME___;
+
+// ___FILEBASENAME___Delegate protocol.
+@protocol ___FILEBASENAMEASIDENTIFIER___Delegate <NSObject>
+@required
+
+@end
